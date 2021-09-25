e15819d2576573041fc527a49cab0ee37a07b28c
Bers <mbers.pro@gmail.com>
2021-09-26 01:53:09 +0300

    added minimum css

--- src/Components/profile/Profile.js
+++ src/Components/profile/Profile.js
@@ -1,24 +1,27 @@
 import React from 'react'; //imr
 import PropTypes from 'prop-types'
+import {container, profile, img, upcontainer, name, some, downcontainer, li, topspan} from './Profile.module.css'
 
 
 const Profile = (props) => {
     return (
-        <div>
-            <div>
-                <img src={props.avatar} alt="Аватар пользователя" />
-                <p>{props.name}</p>
-                <p>{props.tag}</p>
-                <p>{props.location}</p>
+        <div className={container}>
+            <div className={profile}>
+                <div className={upcontainer}>
+                    <img className={img} src={props.avatar} alt="Аватар пользователя" width = "150px"/>
+                    <p className={name}>{props.name}</p>
+                    <p className={some}>@{props.tag}</p>
+                    <p className={some}>{props.location}</p>
+                </div>
+                <ul className={downcontainer}>
+                    {Object.entries(props.stats).map((key, value) =>
+                        <li className={li}>
+                            <span className={topspan}>{key}</span>
+                            <span className={topspan}>{value}</span>
+                        </li>
+                    )}
+                </ul>
             </div>
-            <ul>
-                {Object.entries(props.stats).map((key, value) =>
-                    <li>
-                        <span>{key}</span>
-                        <span>{value}</span>
-                    </li>
-                )}
-            </ul>
         </div>
     );
 }
